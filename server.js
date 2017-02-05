8b4e7a8d1fc5b3f2f3b9ec68fb04e41508160bf1
Andrew Boorde <andrew.boorde@gmail.com>
2017-02-05 03:27:04 +0000

    Finish req parser

--- server.js
+++ server.js
@@ -4,7 +4,17 @@ var path = require("path");
 
 var app = express()
 
-
+app.get('/api/whoami', function(req, res) {
+    var regExp = /\(([^)]+)\)/;
+    var usrOS = regExp.exec(req.headers['user-agent'])[1]
+    var resObj = {
+        ipaddress: req.headers['x-forwarded-for'],
+        language: req.headers['accept-language'].split(',')[0],
+        software: usrOS
+    };
+    console.log(resObj);
+    res.status(200).send(resObj);
+})
 app.listen(process.env.PORT || 8080, function () {
   console.log('Example app listening on port 3000!')
 })
