5d895342ccfdb404e78222b765bf678bc05345af
Andrew Boorde <andrew.boorde@gmail.com>
2017-02-05 02:42:57 +0000

    Init commit

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var path = require("path");
+
+
+var app = express()
+
+
+app.listen(process.env.PORT || 8080, function () {
+  console.log('Example app listening on port 3000!')
+})
